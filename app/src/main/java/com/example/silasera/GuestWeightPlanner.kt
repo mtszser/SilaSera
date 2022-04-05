b37532fc23264796f708aac1bf48b36b32c619a3
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-04-05 18:18:52 +0200

    Changes Themes/New Activities

---
+++ app/src/main/java/com/example/silasera/GuestWeightPlanner.kt
@@ -0,0 +1,11 @@
+package com.example.silasera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuestWeightPlanner : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.guest_weight_planner)
+    }
+}
---
+++ app/src/main/java/com/example/silasera/GuestWomanWorkoutUpper.kt
@@ -0,0 +1,11 @@
+package com.example.silasera
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GuestWomanWorkoutUpper : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.guest_woman_workout_upper)
+    }
+}
