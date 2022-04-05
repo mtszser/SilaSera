e9a5e0a1dbc87de036964cb30cd11436e673bb0e
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-04-05 13:49:39 +0200

    deleted toast

--- app/src/main/java/com/example/silasera/MainActivity.kt
+++ app/src/main/java/com/example/silasera/MainActivity.kt
@@ -17,7 +17,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Firebase connection Success", Toast.LENGTH_LONG).show()
 
 
 
