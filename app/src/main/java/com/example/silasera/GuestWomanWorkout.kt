97c870c14cce1a96581d23487f720b6b96c84adc
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-04-07 15:55:09 +0200

    GuestWorkout Api Done

--- app/src/main/java/com/example/silasera/GuestWomanWorkout.kt
+++ app/src/main/java/com/example/silasera/GuestWomanWorkout.kt
@@ -2,19 +2,24 @@ package com.example.silasera
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.youtube.player.*
 import com.google.firebase.database.*
 
-class GuestWomanWorkout : AppCompatActivity() {
+class GuestWomanWorkout : YouTubeBaseActivity() {
 
     private lateinit var dbref: DatabaseReference
     private lateinit var userRecyclerView: RecyclerView
     private lateinit var userList: ArrayList<User>
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.guest_woman_workout)
+
 
         userRecyclerView = findViewById(R.id.woman_RV)
         userRecyclerView.layoutManager = LinearLayoutManager(this)
@@ -22,7 +27,38 @@ class GuestWomanWorkout : AppCompatActivity() {
 
         userList = arrayListOf<User>()
         getUserData()
+        getYouTubeData()
+
     }
+
+    private fun getYouTubeData() {
+        val ytView = findViewById<YouTubePlayerView>(R.id.yt_view)
+        ytView.initialize(R.string.api_key_.toString(), object:YouTubePlayer.OnInitializedListener{
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                wasRestored: Boolean
+            ) {
+                if(player == null) return
+                if(!wasRestored){
+                    player?.cueVideo("dQw4w9WgXcQ")
+                    player.setPlayerStyle(YouTubePlayer.PlayerStyle.DEFAULT)
+                }
+
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Toast.makeText(this@GuestWomanWorkout, "YouTube Failed", Toast.LENGTH_LONG).show()
+
+            }
+        })
+//        val ytView = findViewById<YouTubePlayerView>(R.id.yt_view)
+//        ytView.initialize(R.string.api_key_.toString(), YouTubePlayer.OnInitializedListener)
+    }
+
 
     private fun getUserData() {
 
@@ -45,4 +81,8 @@ class GuestWomanWorkout : AppCompatActivity() {
 
         })
     }
+
+
+
+
 }
