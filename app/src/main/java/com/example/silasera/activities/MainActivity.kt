cc62a722d15f4c9590abd701fbfabc71057c9cd3
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-04-16 00:28:25 +0200

    Activity Splash

--- app/src/main/java/com/example/silasera/activities/MainActivity.kt
+++ app/src/main/java/com/example/silasera/activities/MainActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
 import android.widget.Toast
+import android.window.SplashScreen
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.silasera.ForgotPassword
 import com.example.silasera.R
 import com.example.silasera.SignUp
@@ -34,6 +36,7 @@ class MainActivity : AppCompatActivity()  {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         firebaseAuth = FirebaseAuth.getInstance()
