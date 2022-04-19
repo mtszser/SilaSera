660c9f1769809527c445732f3d133f71b94f5759
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-04-19 16:17:54 +0200

    BMI Fragment Design

--- app/src/main/java/com/example/silasera/activities/WeightPlanner.kt
+++ app/src/main/java/com/example/silasera/activities/WeightPlanner.kt
@@ -4,33 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.silasera.R
+import com.example.silasera.databinding.FragmentBmiBinding
 import com.example.silasera.databinding.WeightPlannerBinding
 
 class WeightPlanner : AppCompatActivity() {
 
     private lateinit var binding: WeightPlannerBinding
-    private lateinit var NavController: NavController
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = WeightPlannerBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment_cont)
+        setupWithNavController(binding.bottomNavView, navController)
 
-        binding.weightLeftArrow.setOnClickListener{
-            finish()
-        }
+        val fragmentBmi = Fragment(R.layout.fragment_bmi)
 
-        NavController = Navigation.findNavController(this, R.id.nav_host_fragment_cont)
-        setupWithNavController(binding.bottomNavView, NavController)
 
     }
 
     override fun onBackPressed() {
+        finish()
 
     }
 }
--- app/src/main/java/com/example/silasera/fragments/Bmi.kt
+++ app/src/main/java/com/example/silasera/fragments/Bmi.kt
@@ -1,60 +1,39 @@
 package com.example.silasera.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import com.example.silasera.R
+import com.example.silasera.activities.WeightPlanner
+import com.example.silasera.databinding.FragmentBmiBinding
+import com.example.silasera.databinding.WeightPlannerBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [Bmi.newInstance] factory method to
- * create an instance of this fragment.
- */
 class Bmi : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var gender = arrayListOf("Male", "Female")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bmi, container, false)
+        val bmiFragment = inflater.inflate(R.layout.fragment_bmi, container, false)
+
+        val activity = activity as Context
+
+        val spinner = bmiFragment.findViewById<Spinner>(R.id.spinnerBMI)
+        val arrayAdapter = ArrayAdapter<String>(activity, R.layout.support_simple_spinner_dropdown_item, gender, )
+
+        spinner.adapter = arrayAdapter
+        return bmiFragment
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Bmi.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Bmi().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
+
 }
