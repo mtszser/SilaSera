c95827e9e9614166e4825327eb399b37804e2257
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-04-19 11:42:38 +0200

    WeightPlanner new fragments

--- app/src/main/java/com/example/silasera/activities/WeightPlanner.kt
+++ app/src/main/java/com/example/silasera/activities/WeightPlanner.kt
@@ -3,12 +3,18 @@ package com.example.silasera.activities
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.silasera.R
 import com.example.silasera.databinding.WeightPlannerBinding
 
 class WeightPlanner : AppCompatActivity() {
 
     private lateinit var binding: WeightPlannerBinding
+    private lateinit var NavController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,6 +24,9 @@ class WeightPlanner : AppCompatActivity() {
         binding.weightLeftArrow.setOnClickListener{
             finish()
         }
+
+        NavController = Navigation.findNavController(this, R.id.nav_host_fragment_cont)
+        setupWithNavController(binding.bottomNavView, NavController)
 
     }
 
