e4db36b7458e01eeceb07c8b6b2c08f7ba0a9c6a
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-05-07 14:54:09 +0200

    Exoplayer in Cardio Fragment Done

--- app/src/main/java/com/example/silasera/womanWorkout/WomanWorkoutCardio.kt
+++ app/src/main/java/com/example/silasera/womanWorkout/WomanWorkoutCardio.kt
@@ -6,10 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.silasera.R
-import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
+import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.ui.StyledPlayerView
 
 
-class WomanWorkoutCardio : Fragment() {
+class WomanWorkoutCardio : Fragment(), Player.Listener {
+
+    private lateinit var exoPlayer: ExoPlayer
+    private lateinit var exoView: StyledPlayerView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,7 +23,27 @@ class WomanWorkoutCardio : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val wwC = inflater.inflate(R.layout.woman_workout_cardio, container, false)
+        exoView = wwC.findViewById(R.id.cardio_player_view)
+        setUpPlayer(exoView)
+
+
 
         return wwC
+
     }
+
+    private fun setUpPlayer(exoView: StyledPlayerView) {
+        exoPlayer = ExoPlayer.Builder(this.requireContext()).build()
+        exoView.player = exoPlayer
+        exoPlayer.addListener(this)
+
+        addMp4Files(exoPlayer)
+    }
+
+    private fun addMp4Files(exoPlayer: ExoPlayer) {
+        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
+        exoPlayer.addMediaItem(mediaItem)
+        exoPlayer.prepare()
+    }
+
 }
