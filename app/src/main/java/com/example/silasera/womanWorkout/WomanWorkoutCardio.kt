ce8954507a5edb16a0469094f1dfd2cd399701be
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-05-12 15:36:03 +0200

    ProgressBar/Playlist Exoplayer

--- app/src/main/java/com/example/silasera/womanWorkout/WomanWorkoutCardio.kt
+++ app/src/main/java/com/example/silasera/womanWorkout/WomanWorkoutCardio.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import androidx.navigation.Navigation
 import com.example.silasera.R
+import com.example.silasera.dataclass.FreePlaylist
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
@@ -23,6 +25,8 @@ class WomanWorkoutCardio : Fragment(), Player.Listener {
     private lateinit var stopButton: Button
     private lateinit var playButton: Button
     private lateinit var backButton: Button
+    private lateinit var progressBar: ProgressBar
+    private lateinit var cardioPlayList: ArrayList<MediaItem>
 
 
     override fun onCreateView(
@@ -30,8 +34,15 @@ class WomanWorkoutCardio : Fragment(), Player.Listener {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        cardioPlayList = ArrayList()
+        cardioPlayList.add(MediaItem.fromUri("https://storage.googleapis.com/silasera-f7482.appspot.com/ci%C4%85g245_1.mp4"))
+        cardioPlayList.add(MediaItem.fromUri("https://storage.googleapis.com/silasera-f7482.appspot.com/ci%C4%85g245_1.mp4"))
+
+
+
         val wwC = inflater.inflate(R.layout.woman_workout_cardio, container, false)
         exoView = wwC.findViewById(R.id.cardio_player_view)
+        progressBar = wwC.findViewById(R.id.cardio_progress_bar)
         setUpPlayer(exoView)
         setUpButtons(wwC)
 
@@ -89,9 +100,25 @@ class WomanWorkoutCardio : Fragment(), Player.Listener {
 
 
     private fun addMp4Files(exoPlayer: ExoPlayer) {
-        val mediaItem = MediaItem.fromUri(getString(R.string.media_url_mp4))
-        exoPlayer.addMediaItem(mediaItem)
+        for (i in cardioPlayList.indices) {
+            exoPlayer.addMediaItem(cardioPlayList[i])
+        }
         exoPlayer.prepare()
+    }
+
+    override fun onPlaybackStateChanged(playbackState: Int) {
+        super.onPlaybackStateChanged(playbackState)
+
+        when (playbackState) {
+            Player.STATE_BUFFERING -> {
+                exoView.visibility = View.GONE
+                progressBar.visibility = View.VISIBLE
+            }
+            Player.STATE_READY -> {
+                exoView.visibility = View.VISIBLE
+                progressBar.visibility = View.GONE
+            }
+        }
     }
 
 
