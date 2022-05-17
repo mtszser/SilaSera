3223f672248d855f0705f23be8c9f2adf972775b
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-05-17 14:10:37 +0200

    Images for app menu

---
+++ app/src/main/java/com/example/silasera/app/fragments/SendRaport.kt
@@ -0,0 +1,22 @@
+package com.example.silasera.app.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.silasera.R
+
+
+class SendRaport : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.app_send_raport, container, false)
+    }
+
+}
