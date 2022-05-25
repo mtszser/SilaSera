2954d9eab785e36c236eefe5636d6edea28a3303
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-05-25 11:40:41 +0200

    User data class

--- app/src/main/java/com/example/silasera/dataclass/UserProfile.kt
+++ app/src/main/java/com/example/silasera/dataclass/UserProfile.kt
@@ -1,3 +1,6 @@
 package com.example.silasera.dataclass
 
-data class UserProfile(var username: String?, var userlastname: String?, var height: Int?, var weight: Int?)
+
+import android.net.Uri
+
+data class UserProfile(var userName: String?, var userLastname: String?, var userEmail: String?, var userHeight: String?, var userWeight: String?)
