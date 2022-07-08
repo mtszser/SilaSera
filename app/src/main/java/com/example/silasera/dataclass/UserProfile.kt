0480b348f033690b7b4cdeb903c35a754b72ee24
mtszp <mtszserafin@microsoft.wsei.edu.pl>
2022-07-08 20:02:22 +0200

    Added CPM property

--- app/src/main/java/com/example/silasera/dataclass/UserProfile.kt
+++ app/src/main/java/com/example/silasera/dataclass/UserProfile.kt
@@ -5,4 +5,4 @@ import android.net.Uri
 
 data class UserProfile(var userName: String?, var userLastname: String?, var userEmail: String?,
                        var userHeight: Double?, var userWeight: Double?, var userGender: String?,
-                       var userAge: Int, var userBMI: Double)
+                       var userAge: Int, var userBMI: Double, var userCPM: Double?,)
